9f99b6631ec6793be6545368d3410abd48ff9d3e
unknown <sahilsiddiqui.dev@gmail.com>
2020-09-09 13:26:00 +0530

    commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCYMngHa2y8TYViYlsmpvT5nNR97VGRBlU",
+  authDomain: "fir-4a7fe.firebaseapp.com",
+  databaseURL: "https://fir-4a7fe.firebaseio.com",
+  projectId: "fir-4a7fe",
+  storageBucket: "fir-4a7fe.appspot.com",
+  messagingSenderId: "356775774074",
+  appId: "1:356775774074:web:e9d6f8246294e8246d78c3",
+};
