c8824e36183c39b3817ceae4dcc1ecc1e6195b52
unknown <sahilsiddiqui.dev@gmail.com>
2020-09-12 19:42:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Login from "./Login";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+
           <Route path="/checkout">
             <Header />
             <Checkout />
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCYMngHa2y8TYViYlsmpvT5nNR97VGRBlU",
   authDomain: "fir-4a7fe.firebaseapp.com",
@@ -7,3 +9,11 @@ const firebaseConfig = {
   messagingSenderId: "356775774074",
   appId: "1:356775774074:web:e9d6f8246294e8246d78c3",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export { db, auth };
