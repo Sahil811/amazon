7a372828aaef35de7fd187b54b78dc7fd38300ce
unknown <sahilsiddiqui.dev@gmail.com>
2020-09-17 12:26:21 +0530

    commit

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/fir-4a7fe/us-central1/api",
+  //baseURL: "http://localhost:5001/fir-4a7fe/us-central1/api",
+  baseURL: "https://us-central1-fir-4a7fe.cloudfunctions.net/api",
 });
 
 export default instance;
